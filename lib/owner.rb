42387cab4f1ccbb81fe8030a79acd29465808aa7
Chris Cardle <cdcardle@gmail.com>
2017-11-15 17:21:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,4 +28,12 @@ class Owner
   def buy_fish(fish)
     @pets[:fishes] << Fish.new(fish)
   end
+
+  def buy_cat(cat)
+    @pets[:cats] << Cat.new(cat)
+  end
+
+  def buy_dog(dog)
+    @pets[:dogs] << Dog.new(dog)
+  end
 end
